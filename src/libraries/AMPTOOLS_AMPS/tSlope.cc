bb483086b9221aed9394663b6757a6e242b2be64
Justin Stevens <jrsteven@mit.edu>
2015-12-21 07:41:52 -0500

    make tSlope accomadating to any number of daughter particles to compute momentum transfer (eg. both rho and Delta++)

--- src/libraries/AMPTOOLS_AMPS/tSlope.cc
+++ src/libraries/AMPTOOLS_AMPS/tSlope.cc
@@ -14,12 +14,17 @@
 tSlope::tSlope( const vector< string >& args ) :
 UserAmplitude< tSlope >( args )
 {
-	assert( args.size() == 1 );
+	assert( args.size() > 1 );
 	
 	slope  = AmpParameter( args[0] );
-  
+
 	// need to register any free parameters so the framework knows about them
 	registerParameter( slope );
+
+	// set children of resonance to sum
+	for(unsigned int i = 1; i < args.size(); i++) {
+	  resonanceChildren.push_back( atoi( args[i].data() ) );
+	}
 }
 
 
@@ -27,15 +32,15 @@ complex< GDouble >
 tSlope::calcAmplitude( GDouble** pKin ) const {
   
 	TLorentzVector beam   ( pKin[0][1], pKin[0][2], pKin[0][3], pKin[0][0] ); 
-	TLorentzVector recoil ( pKin[1][1], pKin[1][2], pKin[1][3], pKin[1][0] ); 
-	TLorentzVector p1     ( pKin[2][1], pKin[2][2], pKin[2][3], pKin[2][0] ); 
-	TLorentzVector p2     ( pKin[3][1], pKin[3][2], pKin[3][3], pKin[3][0] ); 
-	
-	TLorentzVector resonance = p1 + p2;
-	TLorentzVector target(0, 0, 0, 0.938);
 
-        //GDouble s = (beam + target).M2();
-        GDouble t = (target - recoil).M2();
+	// sum p4 from children of resonace
+	TLorentzVector resonance;
+	for(unsigned int i = 0; i < resonanceChildren.size(); i++) {
+	  TLorentzVector particle ( pKin[resonanceChildren[i]][1], pKin[resonanceChildren[i]][2], pKin[resonanceChildren[i]][3], pKin[resonanceChildren[i]][0] ); 
+	  resonance += particle;
+	}
+
+        GDouble t = (beam - resonance).M2();
 
 	GDouble W = exp(slope * t);
 	
